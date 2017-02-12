9b843840aa5fe28d245b1eebd93e269dd25cd9b8
Mykola Petrivskiy <22659638+Torbins@users.noreply.github.com>
2017-02-12 19:02:16 +0000

    В FLExecutor добавлен код для обработки параметров запуска приложений

--- Executor/FLExecutor.dpr
+++ Executor/FLExecutor.dpr
@@ -3,13 +3,43 @@ program FLExecutor;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils, Winapi.Windows, FLFunctions, FLLanguage, System.Classes;
+
+var
+  WinHandle: HWND;
+  ErrorCode: Integer;
+  LinkStrings: TStringList;
+  Link: lnk;
 
 begin
   try
-    { TODO -oUser -cConsole Main : Insert code here }
+    if ParamCount = 0 then
+    begin
+      if not CreateProcess(ExtractFilePath(ParamStr(0)) + 'FLaunch.exe', '',
+        ExtractFilePath(ParamStr(0)), SW_NORMAL, NORMAL_PRIORITY_CLASS, ErrorCode)
+      then
+        RaiseLastOSError(ErrorCode);
+    end
+    else
+    begin
+      WinHandle := StrToIntDef(ParamStr(2), 0);
+      Language.Load(ParamStr(3));
+
+      LinkStrings := TStringList.Create;
+      try
+        LinkStrings.Delimiter := ';';
+        LinkStrings.QuoteChar := '''';
+        LinkStrings.DelimitedText := ParamStr(1);
+        Link := StringsToLnk(LinkStrings);
+      finally
+        LinkStrings.Free;
+      end;
+
+      ThreadLaunch(Link, WinHandle, ParamStr(4));
+    end;
   except
     on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
+      WarningMessage(WinHandle, StringReplace(e.Message, '%1',
+        ExtractFileName(Link.exec), [rfReplaceAll]));
   end;
 end.
