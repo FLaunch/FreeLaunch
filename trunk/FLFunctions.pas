a7d83e4e2befc7de349f8f7138849bddb8b0199c
torbins <torbins@073c1395-42cd-4c2f-83b3-f71b00cb27cd>
2018-07-27 16:32:40 +0000

    Для изменения размера иконок используется библиотека JCL. Это позволяет корректно обрабатывать прозрачность и повышает качество масштабирования

--- trunk/FLFunctions.pas
+++ trunk/FLFunctions.pas
@@ -132,7 +132,7 @@ implementation
 
 uses
   ShellApi, ShFolder, SysUtils, ActiveX, ComObj, ShlObj, FLLanguage,
-  System.IniFiles, Winapi.CommCtrl;
+  System.IniFiles, Winapi.CommCtrl, jclGraphics;
 
 //--Функция не позволяет уйти значению за пределы допустимых
 //--Входные параметры: значение, минимальное значение, максимальное значение
@@ -271,67 +271,16 @@ end;
 
 //--Функция делает ресайз изображения
 procedure SmoothResize(Src, Dst: TBitmap);
-var
-  x, y: Integer;
-  xP, yP: Integer; 
-  xP2, yP2: Integer;
-  SrcLine1, SrcLine2: pRGBArray;
-  t3: Integer; 
-  z, z2, iz2: Integer; 
-  DstLine: pRGBArray;
-  DstGap: Integer; 
-  w1, w2, w3, w4: Integer;
 begin
-  Src.PixelFormat := pf24Bit;
-  Dst.PixelFormat := pf24Bit; 
-  if (Src.Width = Dst.Width) and (Src.Height = Dst.Height) then
-    Dst.Assign(Src) 
-  else 
-    begin
-      DstLine := Dst.ScanLine[0];
-      DstGap  := Integer(Dst.ScanLine[1]) - Integer(DstLine);
-      xP2 := MulDiv(pred(Src.Width), $10000, Dst.Width);
-      yP2 := MulDiv(pred(Src.Height), $10000, Dst.Height);
-      yP  := 0;
-      for y := 0 to pred(Dst.Height) do
-        begin
-          xP := 0;
-          SrcLine1 := Src.ScanLine[yP shr 16];
-          if (yP shr 16 < pred(Src.Height)) then
-            SrcLine2 := Src.ScanLine[succ(yP shr 16)]
-          else
-            SrcLine2 := Src.ScanLine[yP shr 16];
-          z2  := succ(yP and $FFFF);
-          iz2 := succ((not yp) and $FFFF);
-          for x := 0 to pred(Dst.Width) do
-            begin
-              t3 := xP shr 16;
-              z  := xP and $FFFF;
-              w2 := MulDiv(z, iz2, $10000);
-              w1 := iz2 - w2;
-              w4 := MulDiv(z, z2, $10000);
-              w3 := z2 - w4;
-              DstLine[x].rgbtRed := (SrcLine1[t3].rgbtRed * w1 +
-              SrcLine1[t3 + 1].rgbtRed * w2 +
-              SrcLine2[t3].rgbtRed * w3 + SrcLine2[t3 + 1].rgbtRed * w4) shr 16;
-              DstLine[x].rgbtGreen := (SrcLine1[t3].rgbtGreen * w1 + SrcLine1[t3 + 1].rgbtGreen * w2 +
-              SrcLine2[t3].rgbtGreen * w3 + SrcLine2[t3 + 1].rgbtGreen * w4) shr 16;
-              DstLine[x].rgbtBlue := (SrcLine1[t3].rgbtBlue * w1 +
-              SrcLine1[t3 + 1].rgbtBlue * w2 +
-              SrcLine2[t3].rgbtBlue * w3 +
-              SrcLine2[t3 + 1].rgbtBlue * w4) shr 16;
-              Inc(xP, xP2);
-            end;
-          Inc(yP, yP2);
-          DstLine := pRGBArray(Integer(DstLine) + DstGap);
-        end;
-    end;
+  Dst.PixelFormat := pf32bit;
+  Stretch(Dst.Width, Dst.Height, rfLanczos3, 0, Src, Dst);
+  Dst.AlphaFormat := afDefined;
 end;
 
 //--Нахождение микса двух цветов
 function GetColorBetween(StartColor, EndColor: TColor; Pointvalue, Von, Bis: Extended): TColor;
 var
-  F: Extended; 
+  F: Extended;
   r1, r2, r3, g1, g2, g3, b1, b2, b3: Byte; 
 
   function CalcColorBytes(fb1, fb2: Byte): Byte;
