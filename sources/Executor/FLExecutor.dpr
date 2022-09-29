92cc7ecd16c28b2aecb9520f5c5041374e4f4ff9
Ta2i4 <admin@ta2i4.ru>
2022-09-29 21:12:58 +1000

    added license info in FLExecutor.pas
    Signed-off-by: Ta2i4 <admin@ta2i4.ru>

--- sources/Executor/FLExecutor.dpr
+++ sources/Executor/FLExecutor.dpr
@@ -1,4 +1,28 @@
-﻿program FLExecutor;
+﻿{
+  ##########################################################################
+  #  FreeLaunch 2.7 - free links manager for Windows                       #
+  #  ====================================================================  #
+  #  Copyright (C) 2022 FreeLaunch Team                                    #
+  #  WEB https://github.com/Ta2i4/FreeLaunch                        #
+  #  ====================================================================  #
+  #  This file is part of FreeLaunch.                                      #
+  #                                                                        #
+  #  FreeLaunch is free software: you can redistribute it and/or modify    #
+  #  it under the terms of the GNU General Public License as published by  #
+  #  the Free Software Foundation, either version 3 of the License, or     #
+  #  (at your option) any later version.                                   #
+  #                                                                        #
+  #  FreeLaunch is distributed in the hope that it will be useful,         #
+  #  but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+  #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+  #  GNU General Public License for more details.                          #
+  #                                                                        #
+  #  You should have received a copy of the GNU General Public License     #
+  #  along with FreeLaunch. If not, see <http://www.gnu.org/licenses/>.    #
+  ##########################################################################
+}
+
+program FLExecutor;
 
 {$R *.res}
 
