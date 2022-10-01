1f3f8e616a6f65bb88fe0b1c2ffd24ccb6e9862f
Ta2i4 <admin@ta2i4.ru>
2022-10-02 03:06:05 +1000

    added "makezip" bat file to make zip package with compiled app
    Signed-off-by: Ta2i4 <admin@ta2i4.ru>

---
+++ build/README.md
@@ -0,0 +1,3 @@
+## Note
+
+Delphi Community Edition does not allow compiling source codes from the command line. Therefore, before running scripts to create zip archives or installers, you need to compile the FreeLaunch executables.
---
+++ build/makezip.bat
@@ -0,0 +1,17 @@
+@echo FreeLaunch Copyright (C) 2022 ALexey Tatuyko
+@echo This program comes with ABSOLUTELY NO WARRANTY.
+@echo This is free software, and you are welcome to redistribute it under certain conditions.
+@echo Cleaning garbage
+@echo off
+del /f /q *.zip
+del /f /q "bin\*.drc"
+del /f /q "bin\*.map"
+rd /s /q "bin\help"
+del /f /q "bin\languages\*"
+@echo Copying files
+@echo off
+xcopy "..\localizations\*.lng" "bin\languages" /c /q /r /y
+xcopy "..\docs" "bin\help" /s /c /i /q /r /y
+@echo Adding files to archive
+7zextra\7za.exe a -tzip freelaunch.zip .\bin\*
+pause
